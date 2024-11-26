4ac75903c05d3969902e1624d56cac01dd435695
verigi <zavorykin.dev@yandex.ru>
2024-11-26 21:57:07 +0300

    UPD: add query for findAllByItemId to exclude problems with comments

--- src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
@@ -2,12 +2,14 @@ package ru.practicum.shareit.item.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.comment.entity.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByItemId(Long itemId);
+    @Query("SELECT c FROM Comment c WHERE c.item.id = :itemId")
+    List<Comment> findAllByItemId(@Param("itemId") Long itemId);
 
     @Query("SELECT c FROM Comment c WHERE c.item.id IN :ids")
     List<Comment> findAllCommentsByItemIn(List<Long> ids);
