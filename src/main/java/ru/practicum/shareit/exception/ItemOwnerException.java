5a7953348187d80133601786130844893739ed22
verigi <zavorykin.dev@yandex.ru>
2024-11-26 22:08:44 +0300

    UPD: rename exception

--- src/main/java/ru/practicum/shareit/exception/ItemOwnerException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class ItemOwnerException extends RuntimeException {
-    public ItemOwnerException(final String msg) {
-        super(msg);
-    }
-}
